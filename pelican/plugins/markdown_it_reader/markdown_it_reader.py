6749befa2b0a215c341341475609038ef109eaec
Gaige B. Paulsen <gaige@cluetrust.com>
2021-03-27 19:12:47 -0400

    update to fix image alt issue

--- pelican/plugins/markdown_it_reader/markdown_it_reader.py
+++ pelican/plugins/markdown_it_reader/markdown_it_reader.py
@@ -1,32 +1,32 @@
+from markdown_it import MarkdownIt
+from mdit_py_plugins.deflist import deflist_plugin
+from mdit_py_plugins.footnote import footnote_plugin
+from pygments import highlight
+from pygments.formatters.html import HtmlFormatter
+from pygments.lexers import get_lexer_by_name, guess_lexer
+
 from pelican import signals
 from pelican.readers import BaseReader
 from pelican.utils import pelican_open
 
-from markdown_it import MarkdownIt
-from mdit_py_plugins.footnote import footnote_plugin
-from mdit_py_plugins.deflist import deflist_plugin
-
-from pygments import highlight
-from pygments.lexers import get_lexer_by_name,guess_lexer
-from pygments.formatters.html import HtmlFormatter
-
 
 class MDITReader(BaseReader):
     enabled = True
-    file_extensions = ['md', 'markdown', 'mkd', 'mdown']
+    file_extensions = ["md", "markdown", "mkd", "mdown"]
     extensions = None
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        settings = self.settings['MARKDOWN_IT']
         # TODO: Do actual configuration
-#        settings.setdefault('extension_configs', {})
-#        settings.setdefault('extensions', [])
-#        for extension in settings['extension_configs'].keys():
-#            if extension not in settings['extensions']:
-#                settings['extensions'].append(extension)
-#        if 'markdown.extensions.meta' not in settings['extensions']:
-#            settings['extensions'].append('markdown.extensions.meta')
+
+    #    settings = self.settings["MARKDOWN_IT"]
+    #        settings.setdefault('extension_configs', {})
+    #        settings.setdefault('extensions', [])
+    #        for extension in settings['extension_configs'].keys():
+    #            if extension not in settings['extensions']:
+    #                settings['extensions'].append(extension)
+    #        if 'markdown.extensions.meta' not in settings['extensions']:
+    #            settings['extensions'].append('markdown.extensions.meta')
 
     def read(self, filename):
         with pelican_open(filename) as fp:
@@ -35,7 +35,7 @@ class MDITReader(BaseReader):
         content = None
         metadata = {}
         for i, line in enumerate(text):
-            kv = line.split(':', 1)
+            kv = line.split(":", 1)
             if len(kv) == 2:
                 name, value = kv[0].lower(), kv[1].strip()
                 metadata[name] = self.process_metadata(name, value)
@@ -45,9 +45,14 @@ class MDITReader(BaseReader):
 
         # add footnote and deflist plugins, enable tables
         # add in our processors for links
-        md = MarkdownIt("commonmark").use(footnote_plugin).use(deflist_plugin).enable('table')
+        md = (
+            MarkdownIt("commonmark")
+            .use(footnote_plugin)
+            .use(deflist_plugin)
+            .enable("table")
+        )
 
-        def replace_pelican_placeholdlers( original_url) -> str:
+        def replace_pelican_placeholdlers(original_url) -> str:
             new_url = original_url
             for placeholder in (
                 "author",
@@ -58,51 +63,48 @@ class MDITReader(BaseReader):
                 "static",
                 "attach",
             ):
-                new_url = new_url.replace("%7B" + placeholder + "%7D",
-                                          "{" + placeholder + "}")
-                new_url = new_url.replace("%7C" + placeholder + "%7C",
-                                          "{" + placeholder + "}")
+                new_url = new_url.replace(
+                    "%7B" + placeholder + "%7D", "{" + placeholder + "}"
+                )
+                new_url = new_url.replace(
+                    "%7C" + placeholder + "%7C", "{" + placeholder + "}"
+                )
             return new_url
 
         def render_pelican_link(self, tokens, idx, options, env):
-            tokens[idx].attrSet("href", replace_pelican_placeholdlers(
-                tokens[idx].attrGet("href")))
+            tokens[idx].attrSet(
+                "href", replace_pelican_placeholdlers(tokens[idx].attrGet("href"))
+            )
             # pass token to default renderer.
             return self.renderToken(tokens, idx, options, env)
 
         def render_pelican_image(self, tokens, idx, options, env):
-            tokens[idx].attrSet("src", replace_pelican_placeholdlers(
-                tokens[idx].attrGet("src")))
+            tokens[idx].attrSet(
+                "src", replace_pelican_placeholdlers(tokens[idx].attrGet("src"))
+            )
             # pass token to default renderer.
-            return self.renderToken(tokens, idx, options, env)
+            return self.image(tokens, idx, options, env)
 
         md.add_render_rule("link_open", render_pelican_link)
         md.add_render_rule("image", render_pelican_image)
 
         def get_lexer(info, content):
-            if info and info!='':
+            if info and info != "":
                 lexer = get_lexer_by_name(info)
             else:
                 lexer = guess_lexer(content)
             return lexer
 
-        def render_code_inline(self, tokens, idx, options, env):
-            token = tokens[idx]
-            lexer = get_lexer(token.info, token.content)
-            output = "<code class='codehilite'>"+highlight(token.content, lexer, HtmlFormatter(nowrap=True))+"</code>"
-            return (output)
-
         def render_fence(self, tokens, idx, options, env):
             token = tokens[idx]
             lexer = get_lexer(token.info, token.content)
-            output = highlight(token.content, lexer, HtmlFormatter(cssclass='codehilite',wrapcode=True))
-            return (output)
+            output = highlight(
+                token.content,
+                lexer,
+                HtmlFormatter(cssclass="codehilite", wrapcode=True),
+            )
+            return output
 
-        def render_code_block(self, tokens, idx, options, env):
-            print(f"code type:[{idx}] {tokens[idx]} {tokens[idx].content}")
-            return self.renderToken(tokens, idx, options, env)
-        #md.add_render_rule("code_inline", render_code_inline)
-        #md.add_render_rule("code_block", render_code_block)
         md.add_render_rule("fence", render_fence)
         output = md.render(content)
         return output, metadata
